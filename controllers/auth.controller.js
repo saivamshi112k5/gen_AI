393a076d570b396fab9703d17e6bbc4fe6779cde
saivamshi112k5 <saivamshi2k5@gmail.com>
2026-03-22 14:07:09 +0530

    added login and register endpoints

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,29 +1,83 @@
 const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+
+/* user registeration logic */
 async function RegisterNewUser(req,res){
     const {username,password,email} = req.body;
     if(!username || !password || !email){
-        res.status(400).json({
+        return res.status(400).json({
             message:"please fill all the details"
         })
     }
     const userExists = await userModel.findOne({username:username});
     const emailExists = await userModel.findOne({email:email});
     if(userExists || emailExists){
-        res.status(400).json({
+        return res.status(400).json({
             message: "username or password or both are already under use"
         })
     }
-      const newUser = await userModel.create({
+
+/* hashing the password */
+  const hash = await bcrypt.hash(password,10);
+  const user = new userModel({
     username,
-    password,
-    email
+    email,
+    password: hash
   });
+  await user.save()
+/*creating token */
+  const token = jwt.sign(
+    {id:user._id,username:username},
+    process.env.JWT_SECRET,
+    {expiresIn: "2d"}
+  );
+/*cookie created */
+  res.cookie("token",token);
 
-  return res.status(201).json({
-    message: "user registered successfully",
-    user: newUser
+  res.status(200).json({
+    message:"user registered successfully"
   });
+}
+
+
+async function LoginController(req, res) {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "email isn't registered"
+    });
+  }
+
+  const isMatch =  bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({
+      message: "wrong password"
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "2d" }
+  );
+
+  res.cookie("token", token);
+
+  return res.status(200).json({
+    message: "user logged in successfully",
+    user: {
+      id: user._id,
+      username: user.username
+    }
+  });
+}
+module.exports = {
+  RegisterNewUser,
+  LoginController
 };
-
-module.exports = {RegisterNewUser};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,5 +3,6 @@ const authController = require("../controllers/auth.controller");
 const authRouter = express.Router();
 
 authRouter.post("/register",authController.RegisterNewUser);
+authRouter.post("/login",authController.LoginController);
 
 module.exports = authRouter;
